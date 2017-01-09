b391e634cefa8f545241354262c7a6ffcf944bb3
Kerry D Stiles <KerryDStiles@gmail.com>
2017-01-09 15:31:52 -0800

    Update caesar_cipher.rb

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -2,20 +2,22 @@
 #Kerry Stiles
 #Ruby Project 12/07/2017
 
-puts "Please enter the text to be passed through the cipher: "
-code = gets.split("")
-
 puts "Please enter your cipher shift: "
 shift = gets.to_i
 
-code.each {|x| print x}
-puts shift
-
-#Take Input
-
-#Parse the string into a array
-
-#Take each letter and convert to its ASCII number
-
-#Add 5 to move 5 letter down the alphabet (Z wraps to A)
-
+puts "Please enter the text to be passed through the cipher: "
+code = gets.each_byte do |c|
+    if shift > 0 && c === 90
+       c = 65 + (shift - 1)
+    elsif shift < 0 && c === 65
+        c = 90 + (shift + 1)
+    elsif shift > 0 && c === 122
+        c = 97 + (shift - 1)
+    elsif shift < 0 && c === 97
+        c = 122 + (shift + 1)
+    else
+    c = c + shift
+    end
+    
+    print c.chr
+end
