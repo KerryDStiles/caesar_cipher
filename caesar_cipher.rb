8e9ab78c7c0013eabdb8f971dee8bf7b347ea826
Kerry D Stiles <KerryDStiles@gmail.com>
2017-01-09 16:36:08 -0800

    Update caesar_cipher.rb

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -7,17 +7,39 @@ shift = gets.to_i
 
 puts "Please enter the text to be passed through the cipher: "
 code = gets.each_byte do |c|
-    if shift > 0 && c === 90
-       c = 65 + (shift - 1)
-    elsif shift < 0 && c === 65
-        c = 90 + (shift + 1)
-    elsif shift > 0 && c === 122
-        c = 97 + (shift - 1)
-    elsif shift < 0 && c === 97
-        c = 122 + (shift + 1)
+    if c === 32 || c < 65 || c.between?(91,96) || c > 122
+        print c.chr
     else
-    c = c + shift
+        if shift > 0 && c === 90 #Z rolls to A
+            c = 65 + (shift - 1)
+        elsif shift < 0 && c === 65 #A rolls back to Z
+            c = 90 + (shift + 1)
+        elsif shift > 0 && c === 122 #z rolls to a
+            c = 97 + (shift - 1)
+        elsif shift < 0 && c === 97 #a rolls back to z
+            c = 122 + (shift + 1)
+        else
+        c = c + shift
+        end
     end
     
-    print c.chr
+    case                    #isolating for letters
+        when c < 65         #When ASCII is under 65
+            while c < 65    #Add 64 until the number is above 65
+                c += 64
+            end
+            print c.chr
+        when c.between?(91, 96) #When ASCII is under 65
+            c += 6  
+            print c.chr
+        when c > 122        #When ACSII is over 122
+            while c > 122
+                c -= 64     #cyle 64 out until it is under 122
+            end
+            print c.chr
+        else
+            print c.chr
+    end
+        
+    
 end
